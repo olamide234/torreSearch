ca2f1eea6f43f24b50bb6eadb625d5adb612cef7
Olamide Tofade <tofade.olamide2@gmail.com>
2023-10-08 20:23:16 +0100

    page title added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="bg-[#010101] min-h-screen">
-      
+      <div className="flex items-center flex h-14 w-full bg-[#27292d] pr-2">
+        <h1 className="mx-4 text-xl pb-2 text-[#ffffffe6]">Search</h1>
+      </div>
     </main>
   )
 }
